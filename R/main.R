2dffdedca115cf81bd3073d118fa6d2428f3a586
Joe Cheng <joe@rstudio.com>
2016-03-24 12:27:17 -0700

    Initial commit

---
+++ R/main.R
@@ -0,0 +1,69 @@
+#' Run a program asynchronously
+#'
+#' Like system2(wait=FALSE), but gives you a way to get the exit code.
+#'
+#' @examples
+#'
+#' resultPromise <- system2.5("ls", c(Sys.getenv("HOME"), "baddir"))
+#'
+#' resultPromise$then(function(result) {
+#'   cat(paste(result$stdout, collapse = "\n"), "\n")
+#'   message(paste(result$stderr, collapse = "\n"), "\n")
+#'   cat("\nExited with status", result$exitstatus, "\n")
+#' })
+#'
+#' Sys.sleep(1)
+#' shiny:::timerCallbacks$executeElapsed()
+#' shiny:::flushReact()
+#'
+#' @import shiny
+#' @export
+system2.5 <- function(command, args = character(), input = NULL,
+  env = character(), invisible = TRUE) {
+
+  outdir <- tempfile()
+  if (!dir.create(outdir, mode = "0700")) {
+    stop("Failed to create temp dir for job monitoring")
+  }
+
+  callbacks <- NULL
+  result <- list(
+    then = function(cb) {
+      callbacks <<- c(cb, callbacks)
+    }
+  )
+
+  o <- observe({
+    if (!file.exists(file.path(outdir, "exitcode"))) {
+      invalidateLater(250, NULL)
+    } else {
+      o$destroy()
+      if (nchar(outdir) < nchar(tempdir())) {
+        stop("assertion failed")
+      }
+
+      exitstatus <- as.numeric(readLines(file.path(outdir, "exitcode")))
+      stdout <- readLines(file.path(outdir, "job.stdout"))
+      stderr <- readLines(file.path(outdir, "job.stderr"))
+
+      on.exit(unlink(outdir, recursive = TRUE), add = TRUE)
+      for (callback in callbacks) {
+        callback(list(
+          exitstatus = exitstatus,
+          stdout = stdout,
+          stderr = stderr
+        ))
+      }
+    }
+  })
+
+  system2(
+    system.file("run.sh", package = "system2.5"),
+    args = c(outdir, command, args),
+    input = input,
+    env = env,
+    wait = FALSE
+  )
+
+  result
+}
---
+++ inst/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# First argument is the output dir where we'll put job info files
+OUTDIR="$1"
+shift
+
+# Record the pid of the bash process itself
+echo "$$" > "$OUTDIR/ppid.tmp"
+mv "$OUTDIR/ppid.tmp" "$OUTDIR/ppid"
+
+# Second argument is the command, sans arguments
+PROG="$1"
+shift
+
+# Run the program in the background, with stdout/stderr redirected.
+# We have to run in the background so that we can save the PID to
+# a file right away.
+$PROG "$@" > "$OUTDIR/job.stdout" 2> "$OUTDIR/job.stderr" <&0 &
+PID="$!"
+
+# Record the PID
+echo "$PID" > "$OUTDIR/pid.tmp"
+mv "$OUTDIR/pid.tmp" "$OUTDIR/pid"
+
+wait %1
+STATUS="$?"
+
+# Record the exit code
+echo "$STATUS" > "$OUTDIR/exitcode.tmp"
+mv "$OUTDIR/exitcode.tmp" "$OUTDIR/exitcode"
+
+# We ourselves will exit with the same exit code as the program
+exit $STATUS
